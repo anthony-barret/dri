0c8374f169590576df1f84ad4d7c20b79a35115f
Anthony BARRET <47778402+anthony-barret@users.noreply.github.com>
2024-11-11 09:07:42 +0100

    feat: :sparkles: add DownloadImage

--- download-reddit-images/dri.go
+++ download-reddit-images/dri.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"strings"
 )
 
@@ -67,4 +68,28 @@ func GetImagesLinksFromSubreddit(subredditLink string) []string {
 		log.Fatalln(err)
 	}
 	return redditJsonPage.GetLinks()
+}
+
+func DownloadImage(imageLink string) {
+	response, err := http.Get(imageLink)
+	if err != nil {
+		log.Fatalln("Error when requesting", imageLink)
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fileName := "img/" + path.Base(imageLink)
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatalln("Error creating file", err)
+		return
+	}
+	defer file.Close()
+	_, err = file.Write(body)
+	if err != nil {
+		log.Fatalln("Error writing in file", err)
+		return
+	}
 }
