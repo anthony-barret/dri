40811348efa64b6122a261c3f4bc4a7ef2d843fe
Anthony BARRET <47778402+anthony-barret@users.noreply.github.com>
2024-12-06 17:44:13 +0100

    feat: :sparkles: filter NSFW content according to user config

--- download-reddit-images/rr.go
+++ download-reddit-images/rr.go
@@ -6,15 +6,19 @@ type RedditResponse struct {
 	Data struct {
 		Children []struct {
 			Data struct {
-				URL string `json:"url"`
+				URL    string `json:"url"`
+				Over18 bool   `json:"over_18"`
 			} `json:"data"`
 		} `json:"children"`
 	} `json:"data"`
 }
 
-func (rr RedditResponse) GetLinks() []string {
+func (rr RedditResponse) GetLinks(config Config) []string {
 	var links []string
 	for _, child := range rr.Data.Children {
+		if !config.Config.Over18 && child.Data.Over18 {
+			continue
+		}
 		if strings.HasSuffix(child.Data.URL, ".jpg") || strings.HasSuffix(child.Data.URL, ".jpeg") || strings.HasSuffix(child.Data.URL, ".png") {
 			links = append(links, string(child.Data.URL))
 		}
