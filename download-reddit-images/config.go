7de19e161f0adbe5e3a0c54d4feded38454ad090
Anthony BARRET <47778402+anthony-barret@users.noreply.github.com>
2024-12-07 23:38:59 +0100

    feat: :sparkles: add configuration possibility to sort feeds by different criteria

--- download-reddit-images/config.go
+++ download-reddit-images/config.go
@@ -1,6 +1,7 @@
 package downloadredditimages
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -8,10 +9,28 @@ import (
 	yaml "gopkg.in/yaml.v3"
 )
 
+type SortBy string
+
+func (sb *SortBy) UnmarshalYAML(value *yaml.Node) error {
+	var allowedValues = []string{"hot", "new", "top", "rising"}
+	var rawValue string
+	if err := value.Decode(&rawValue); err != nil {
+		return err
+	}
+	for _, allowed := range allowedValues {
+		if rawValue == allowed {
+			*sb = SortBy(rawValue)
+			return nil
+		}
+	}
+	return fmt.Errorf("invalid value %q. The allowed values are: %v", rawValue, allowedValues)
+}
+
 type Config struct {
 	Config struct {
 		Directory string `yaml:"directory"`
 		Limit     int    `yaml:"limit"`
+		SortBy    SortBy `yaml:"sort_by"`
 		Over18    bool   `yaml:"over_18"`
 	} `yaml:"config"`
 	SubReddits []string `yaml:"subreddits"`
@@ -29,7 +48,7 @@ func ParseConfig(configFile string) Config {
 	}
 	links := make([]string, 0)
 	for _, sub := range config.SubReddits {
-		link := "https://www.reddit.com/r/" + sub + "/new.json?limit=" + strconv.Itoa(config.Config.Limit)
+		link := "https://www.reddit.com/r/" + sub + "/" + string(config.Config.SortBy) + ".json?limit=" + strconv.Itoa(config.Config.Limit)
 		links = append(links, link)
 	}
 	config.SubReddits = links
